556a1c1213f34fd7d5be75759125b3da2abfd7cd
Richard-Younes <richard.younes@lau.edu>
2024-02-16 22:44:08 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+/** @format */
+
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Test() {
+	const [movieRating, setMovieRating] = useState(0);
+
+	return (
+		<div>
+			<StarRating maxRating={5} color='blue' onSetRating={setMovieRating} />
+			<p>This movie was rated {movieRating} stars</p>
+		</div>
+	);
+}
+root.render(
+	<React.StrictMode>
+		{/* <App /> */}
+		<StarRating
+			maxRating={5}
+			messages={['Terrible', 'bad', 'medium', 'sexy', 'Beast']}
+		/>
+		<StarRating
+			maxRating={7}
+			color='green'
+			size={24}
+			className='test'
+			defaultRating={2}
+		/>
+		<Test />
+	</React.StrictMode>
+);
