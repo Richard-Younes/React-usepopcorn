be2a3b4af78aafb61ceb8d500a4be309989a82bb
Richard-Younes <richard.younes@lau.edu>
2024-03-05 17:54:51 +0200

    Added the local Storage functionality

--- src/index.js
+++ src/index.js
@@ -2,35 +2,13 @@
 
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Test() {
-	const [movieRating, setMovieRating] = useState(0);
-
-	return (
-		<div>
-			<StarRating maxRating={5} color='blue' onSetRating={setMovieRating} />
-			<p>This movie was rated {movieRating} stars</p>
-		</div>
-	);
-}
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<StarRating
-			maxRating={5}
-			messages={['Terrible', 'bad', 'medium', 'sexy', 'Beast']}
-		/>
-		<StarRating
-			maxRating={7}
-			color='green'
-			size={24}
-			className='test'
-			defaultRating={2}
-		/>
-		<Test />
+		<App />
 	</React.StrictMode>
 );
